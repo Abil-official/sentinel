6103874c391a51e04829cad1ad5fda66f84cee50
taylorotwell <463230+taylorotwell@users.noreply.github.com>
2026-01-20 21:41:26 +0000

    Update facade docblocks

--- src/Sentinel.php
+++ src/Sentinel.php
@@ -5,14 +5,14 @@ namespace Laravel\Sentinel;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static \Illuminate\Contracts\Container\Container getContainer()
- * @method static \Laravel\Sentinel\SentinelManager extend(string $driver, \Closure $callback)
- * @method static \Laravel\Sentinel\SentinelManager forgetDrivers()
- * @method static \Laravel\Sentinel\SentinelManager setContainer(\Illuminate\Contracts\Container\Container $container)
- * @method static array getDrivers()
- * @method static mixed driver(string|null $driver = null)
- * @method static mixed driverOrFallback(string|null $driver)
  * @method static string|null getDefaultDriver()
+ * @method static mixed driverOrFallback(string|null $driver)
+ * @method static mixed driver(string|null $driver = null)
+ * @method static \Laravel\Sentinel\SentinelManager extend(string $driver, \Closure $callback)
+ * @method static array getDrivers()
+ * @method static \Illuminate\Contracts\Container\Container getContainer()
+ * @method static \Laravel\Sentinel\SentinelManager setContainer(\Illuminate\Contracts\Container\Container $container)
+ * @method static \Laravel\Sentinel\SentinelManager forgetDrivers()
  *
  * @see \Laravel\Sentinel\SentinelManager
  */
