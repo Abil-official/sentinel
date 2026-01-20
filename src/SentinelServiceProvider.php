80d884e77fe5b6d52f7cf2fc3c76abfe5e81bf19
Mior Muhammad Zaki <crynobone@gmail.com>
2026-01-20 09:51:41 +0800

    Remove registering alias by default
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/SentinelServiceProvider.php
+++ src/SentinelServiceProvider.php
@@ -4,7 +4,6 @@ namespace Laravel\Sentinel;
 
 use Illuminate\Routing\Router;
 use Illuminate\Support\ServiceProvider;
-use Laravel\Sentinel\Http\Middleware\SentinelMiddleware;
 
 class SentinelServiceProvider extends ServiceProvider
 {
@@ -14,9 +13,5 @@ class SentinelServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->singleton(SentinelManager::class, fn ($app) => new SentinelManager($app));
-
-        $this->callAfterResolving('router', function (Router $router) {
-            $router->aliasMiddleware('sentinel', SentinelMiddleware::class);
-        });
     }
 }
--- workbench/app/Providers/WorkbenchServiceProvider.php
+++ workbench/app/Providers/WorkbenchServiceProvider.php
@@ -4,7 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Contracts\Http\Kernel as HttpKernel;
 use Illuminate\Http\Middleware\TrustProxies;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Sentinel\Http\Middleware\SentinelMiddleware;
 
 use function Orchestra\Testbench\laravel_version_compare;
 
@@ -20,6 +22,8 @@ class WorkbenchServiceProvider extends ServiceProvider
                 $kernel->prependMiddleware(TrustProxies::class);
             });
         }
+
+        Route::aliasMiddleware('sentinel', SentinelMiddleware::class);
     }
 
     /**
